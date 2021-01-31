fbdb0783a4caa85dbe188c33d3f42ac1ebb88dd9
Kai Tang <16kt16@queensu.ca>
2021-01-31 08:58:09 -0500

    406 updated

--- Jan31/406MinimumSizeSubarraySum.java
+++ Jan31/406MinimumSizeSubarraySum.java
@@ -34,3 +34,85 @@ public class Solution {
 
     
 }
+
+
+public class Solution2 {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+
+        // O(n2)
+
+        int minLen = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            int sum=0;
+            for(int j=i; j<nums.length; j++){
+                sum += nums[j];
+                if(sum>=s){
+                    minLen =Math.min(j-i+1, minLen);
+                    break;
+                }
+            }
+        }
+
+        if(minLen<Integer.MAX_VALUE){
+            return minLen;
+        }else{
+            return -1;
+        }
+
+
+
+    }
+
+    
+}
+
+
+public class Solution3 {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+
+        // O(n2)
+
+        int minLen = Integer.MAX_VALUE;
+
+        int sum = 0;
+        int j = 0;
+
+        for(int i=0; i<nums.length; i++){
+
+
+            while(j<nums.length && sum<s){
+                sum += nums[j];
+                j++;
+            }
+
+            if(sum>=s){
+                minLen =Math.min(j-i+1, minLen);
+                sum -= nums[i];
+            }
+        }
+
+        if(minLen<Integer.MAX_VALUE){
+            return minLen;
+        }else{
+            return -1;
+        }
+
+
+
+    }
+
+    
+}
