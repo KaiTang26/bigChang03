2ae27f947748562c84f477f4203a0248f88c937e
Kai Tang <16kt16@queensu.ca>
2021-02-09 22:48:34 -0500

    unionFind

---
+++ Feb11/unionFind.java
@@ -0,0 +1,27 @@
+package Feb11;
+
+public class unionFind {
+
+    public int[] father;
+
+    public int find(int a){
+
+        if(father[a]==a){
+            return a;
+        }
+
+        return father[a]=find(father[a]);
+    }
+
+    public void union(int a, int b){
+
+        int aRoot = find(a);
+
+        int bRoot = find(b);
+
+        if(aRoot ==bRoot){
+            father[aRoot]=father[bRoot];
+        }
+    }
+    
+}
