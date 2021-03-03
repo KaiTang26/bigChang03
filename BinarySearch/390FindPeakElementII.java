b5c7517dc539ef054ccbb99241816f64d6e0d9ea
Kai Tang <16kt16@queensu.ca>
2021-03-02 23:21:42 -0500

    390

--- BinarySearch/390FindPeakElementII.java
+++ BinarySearch/390FindPeakElementII.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
     /**
      * @param A: An integer matrix
@@ -52,5 +55,102 @@ public class Solution {
             }
         }
         return index;
+    }
+}
+
+
+
+public class Solution_2 {
+    /**
+     * @param A: An integer matrix
+     * @return: The index of the peak
+     */
+
+
+
+    public List<Integer> findPeakII(int[][] A) {
+        // write your code here
+        int n = A.length;
+        int m = A[0].length;
+        return find(1, n-2, 1, m-2, A);
+    }
+
+
+    public List<Integer> find(int xs, int xe, int ys, int ye, int[][] A){
+
+        int midX = xs+(xe-xs)/2;
+
+        int midY = ys+(ye-ys)/2;
+
+        int x = midX;
+
+        int y = midY;
+
+        int max = A[midX][midY];
+
+
+       
+
+        // find max along y
+        for(int i=ys; i<=ye; ++i){
+            if(A[midX][i]>max){
+                max = A[midX][i];
+                y = i;
+                x = midX;
+            }
+        }
+
+         // find max along x
+         for(int i=xs; i<=xe; ++i){
+            if(A[i][midY]>max){
+                max = A[i][midY];
+                x = i;
+                y = midY;
+            }
+        }
+
+        boolean isPeak = true;
+
+        if(A[x][y-1]>A[x][y]){
+            isPeak = false;
+            y -=1;
+        }else if(A[x][y+1]>A[x][y]){
+            isPeak =false;
+            y +=1;
+        }else if(A[x-1][y]>A[x][y]){
+            isPeak = false;
+            x -=1;
+        }else if(A[x+1][y]>A[x][y]){
+            x +=1;
+            isPeak =false;
+        }
+
+
+        if(isPeak){
+            return new ArrayList<Integer>(Arrays.asList(x,y));
+        }
+
+
+        if(x>=xs && x<midX && y>=ys && y<midY){
+            return find(xs, midX-1, ys, midY-1, A);
+        }
+
+        if(x>=xs && x<midX && y<=ye && y>midY){
+            return find(xs, midX-1, midY+1, ye, A);
+        }
+
+        if(x<=xe && x>midX && y>=ys && y<midY){
+            return find(midX+1, xe, ys, midY-1, A);
+        }
+
+        if(x<=xe && x>midX && y<=ye && y<midY){
+            return find(midX+1, xe, midY+1, ye, A);
+        }  
+        
+      
+     
+
+        return new ArrayList<Integer>(Arrays.asList(-1,-1));
+
     }
 }
