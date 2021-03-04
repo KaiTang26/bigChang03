f3ac9b74ceaa409ed43079a02bbb1240af9ab95c
Kai Tang <16kt16@queensu.ca>
2021-03-04 11:36:17 -0500

    141

---
+++ BinarySearch/141Sqrt(x).java
@@ -0,0 +1,32 @@
+public class Solution {
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    public int sqrt(int x) {
+        // write your code here
+        if(x==0){
+            return 0;
+        }
+        long start = 1;
+        long end = x;
+
+        while(start+1<end){
+            long mid = start + (end-start)/2;
+            
+            if(mid*mid >x){
+                end=mid;
+            }else if(mid*mid<x){
+                start = mid;
+            }else{
+                start = mid;
+            }
+        }
+
+        if(end*end<=x){
+            return (int)end;
+        }
+
+        return (int)start;
+    }
+}
