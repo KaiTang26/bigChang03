1ec7daf9d1ac6f071cd96a9fdcbe1b72424c0554
Kai Tang <16kt16@queensu.ca>
2021-03-08 17:52:43 -0500

    131

---
+++ sweep-line/131TheSkylineProblem.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param buildings: A list of lists of integers
+     * @return: Find the outline of those buildings
+     */
+    public List<List<Integer>> buildingOutline(int[][] buildings) {
+        // write your code here
+    }
+}
