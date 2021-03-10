7d601097313072806163f926e53956c5fa918971
Kai Tang <16kt16@queensu.ca>
2021-03-09 20:42:34 -0500

    update 392

--- DP/392HouseRobber.java
+++ DP/392HouseRobber.java
@@ -21,3 +21,29 @@ public class Solution {
         return dp[A.length];
     }
 }
+
+
+
+public class Solution2 {
+    /**
+     * @param A: An array of non-negative integers
+     * @return: The maximum amount of money you can rob tonight
+     */
+    public long houseRobber(int[] A) {
+        // write your code here
+
+        if(A==null||A.length==0){
+            return 0;
+        }
+
+        long [] dp = new long [3];
+        dp[0]=0;
+
+        for(int i=0; i<A.length; i++){
+
+            dp[(i+1)%3] = Math.max(dp[i%3], i-1>=0?A[i]+dp[(i-1)%3]:A[i]);
+        }
+
+        return dp[(A.length)%3];
+    }
+}
