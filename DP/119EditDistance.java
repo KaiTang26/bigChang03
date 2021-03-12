89c15925f71d5232697fbdd71e3999bd585444ac
Kai Tang <16kt16@queensu.ca>
2021-03-12 15:22:30 -0500

    119

--- DP/119EditDistance.java
+++ DP/119EditDistance.java
@@ -39,3 +39,46 @@ class Solution {
 
     }
 }
+
+
+class Solution2 {
+    /**
+     * @param word1: A string
+     * @param word2: A string
+     * @return: The minimum number of steps.
+     */
+    public int minDistance(String word1, String word2) {
+        // write your code here
+
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[2][n+1];
+
+
+
+        for(int i=0; i<=m; i++){
+
+            dp[i%2][0] = i;
+
+            for(int j=1; j<=n; j++){
+                if(i>0){
+
+                    if(word1.charAt(j-1)==word2.charAt(i-1)){
+                        dp[i%2][j] = dp[(i-1)%2][j-1];
+                    }else {
+                        dp[i%2][j] = 1 + (Math.min(Math.min(dp[(i - 1)%2][j], dp[i%2][j - 1]), dp[(i - 1)%2][j - 1]));
+                    }
+
+                }else{
+                    dp[i%2][j] = j;
+                }
+
+            }
+        }
+
+        return dp[m%2][n];
+
+
+    }
+}
