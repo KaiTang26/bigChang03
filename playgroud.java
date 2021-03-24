13032765f730051a471f92dc4e43ecbdc796be38
Kai Tang <16kt16@queensu.ca>
2021-03-23 20:32:02 -0400

    600

--- playgroud.java
+++ playgroud.java
@@ -1,59 +1,144 @@
 public class Solution {
-    /**
-     * @param matrix: matrix, a list of lists of integers
-     * @param target: An integer
-     * @return: a boolean, indicate whether matrix contains target
-     */
-    public boolean searchMatrix(int[][] matrix, int target) {
+    
+    public int minArea(char[][] image, int x, int y) {
         // write your code here
 
-        if(matrix==null || matrix.length==0){
-            return false;
+        if(image==null || image.length==0 || image[0].length==0){
+            return 0;
         }
 
-        int startRow = 0;
-        int endRow = matrix.length-1;
+        int left = getLeft(image, 0, y);
 
-        int row = 0;
+        int right = getRight(image, y, image[0].length-1);
 
+        int up = getTop(image, 0, x);
 
-        while(startRow+1<endRow){
-            
-            int mid = startRow+(endRow-startRow)/2;
+        int down = getDown(image, x, image.length-1);
 
-            if(matrix[mid][0]>target){
-                endRow = mid;
-            }else{
-                startRow = mid;
-            }
-        }
+        return (right-left+1)*(down-up+1);
+    }
 
+    private int getLeft(char[][] image, int left, int right){
 
-        if(matrix[endRow][0]<=target){
-            row = endRow;
-        }else{
-            row = startRow;
-        }
-
-        int start = 0;
-        int end = matrix[0].length-1;
+        int start = left;
+        int end = right;
 
         while(start+1<end){
+
             int mid = start + (end-start)/2;
 
-            if(matrix[row][mid]>target){
+            if(checkCol(image, mid)){
                 end = mid;
             }else{
-                start=mid;
+                start = mid;
+            }
+
+        }
+
+        if(checkCol(image, start)){
+            return start;
+        }else{
+            return end;
+        }
+
+    }
+
+    private int getRight(char[][] image, int left, int right){
+        int start = left;
+        int end = right;
+
+        while(start+1<end){
+
+            int mid = start + (end-start)/2;
+
+            if(checkCol(image, mid)){
+                start = mid;
+            }else{
+                end = mid;
+            }
+
+        }
+
+        if(checkCol(image, end)){
+            return end;
+        }else{
+            return start;
+        }
+
+    }
+
+    private int getTop(char[][] image, int top, int down){
+
+        int start = top;
+        int end = down;
+
+        while(start+1<end){
+            int mid = start+(end-start)/2;
+
+            if(checkRow(image, mid)){
+                end = mid;
+            }else{
+                start = mid;
             }
         }
 
-        if(matrix[row][start]==target || matrix[row][end]==target){
-            return true;
+        if(checkRow(image, start)){
+            return start;
         }else{
-            return false;
+            return end;
         }
 
-       
     }
+
+    private int getDown(char[][] image, int top, int down){
+
+        int start = top;
+        int end = down;
+
+        while(start+1<end){
+
+            int mid = start+(end-start)/2;
+
+            if(checkRow(image, mid)){
+                start = mid;
+            }else{
+                end = mid;
+            }
+
+        }
+
+        if(checkRow(image, end)){
+            return end;
+        }else{
+            return start;
+        }
+
+    }
+
+    private boolean checkRow(char[][] image, int row){
+
+        for(char i: image[row]){
+            if(i=='1'){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    private boolean checkCol(char[][] image, int col){
+
+        for(int i=0; i<image.length; i++){
+
+            if(image[i][col]=='1'){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+
 }
