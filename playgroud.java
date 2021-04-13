a7d035d7ba02ba932a814c454c09bea21d448181
Kai Tang <16kt16@queensu.ca>
2021-04-13 15:47:59 -0400

    97

--- playgroud.java
+++ playgroud.java
@@ -1,50 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class TreeNode {
-      public int val;
+/**
+ * Definition of TreeNode:
+  */
+  public class TreeNode {
+     public int val;
       public TreeNode left, right;
-      public TreeNode(int val) {
+     public TreeNode(int val) {
           this.val = val;
           this.left = this.right = null;
       }
   }
 
 
-  public class Solution {
+public class Solution {
     /**
-     * @param root: the root of the binary tree
-     * @return: all root-to-leaf paths
+     * @param root: The root of binary tree.
+     * @return: An integer
      */
-   
-    public List<String> binaryTreePaths(TreeNode root) {
+    public int maxDepth(TreeNode root) {
         // write your code here
 
-        List<String> ans = new ArrayList<>();
+        return DFS(root, 0);
 
-        if(root==null){
-            return ans;
+
+    }
+
+    public int DFS(TreeNode node, int depth){
+
+        if(node==null){
+            return depth;
         }
 
-        List<String> left = binaryTreePaths(root.left);
 
-        List<String> right = binaryTreePaths(root.right);
-
-        for(String i: left){
-            String path = root.val+"->"+i;
-            ans.add(path);
-        }
-
-        for(String i: right){
-            String path = root.val+"->"+i;
-            ans.add(path);
-        }
-
-        if(left.size()==0 && right.size()==0){
-            ans.add(Integer.toString(root.val));
-        }
-
-        return ans;
+        return Math.max(DFS(node.left, depth+1), DFS(node.right, depth+1));
 
     }
 }
