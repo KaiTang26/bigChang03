3782c638ac1893e363af295a849ffa3b49169d61
Kai Tang <16kt16@queensu.ca>
2021-04-15 13:58:39 -0400

    178

--- playgroud.java
+++ playgroud.java
@@ -1,71 +1,74 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
-import java.util.Stack;
 
-import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+public class Solution {
+    /**
+     * @param n: An integer
+     * @param edges: a list of undirected edges
+     * @return: true if it's a valid tree, or false
+     */
+    public boolean validTree(int n, int[][] edges) {
+        // write your code here
 
-class TreeNode {
-    public int val;
-    public TreeNode left, right;
-    public TreeNode(int val) {
-        this.val = val;
-        this.left = this.right = null;
-    }
-}
+        int numberofEdges = edges.length;
 
-class ListNode {
-         int val;
-        ListNode next;
-         ListNode(int x) { val = x; }
-     }
+        if(numberofEdges != (n-1)){
+            return false;
+        }
 
-     public class Solution {
-        /**
-         * @param root the root of binary tree
-         * @return a lists of linked list
-         */
-        public List<ListNode> binaryTreeToLists(TreeNode root) {
-            // Write your code here
 
-            List<ListNode> ans = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
-            if(root==null){
-                return ans;
-            }
+        for(int i=0; i<n; i++){
 
-            Queue<TreeNode> queue = new LinkedList<>();
 
-            queue.offer(root);
+            map.put(i, new ArrayList<>());
 
-            while(!queue.isEmpty()){
-                int size = queue.size();
 
-                ListNode dummy = new ListNode(-1);
-                ListNode current = dummy;
+        }
 
-                for(int i=0; i<size; i++){
+        for(int i=0; i<numberofEdges; i++){
+            int[] edge =edges[i];
 
-                    TreeNode node = queue.poll();
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
 
-                    current.next = new ListNode(node.val);
+        }
 
-                    if(node.left !=null){
-                        queue.offer(node.left);
-                    }
 
-                    if(node.right != null){
-                        queue.offer(node.right);
-                    }
 
-                    current = current.next;
 
+
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        Set<Integer> set = new HashSet<>();
+
+        queue.offer(0);
+        set.add(0);
+
+
+        while(!queue.isEmpty()){
+            int key = queue.poll();
+
+
+            List<Integer> values = map.get(key);
+
+            for(int i:values){
+
+
+                if(!set.contains(i)){
+                    queue.offer(i);
+                    set.add(i);
                 }
 
-                ans.add(dummy.next);
             }
 
-
-            return ans;
         }
+
+       
+
+        return set.size()==n;
     }
+}
