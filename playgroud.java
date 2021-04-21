0520b965524f56f2c0df699fd2ecb409c493eb5d
Kai Tang <16kt16@queensu.ca>
2021-04-21 08:34:24 -0400

    18

--- playgroud.java
+++ playgroud.java
@@ -1,35 +1,39 @@
 import java.awt.List;
-import java.util.ArrayList;
 
 public class Solution {
     /**
-     * @param nums: A set of numbers
-     * @return: A list of lists
+     * @param nums: A set of numbers.
+     * @return: A list of lists. All valid subsets.
      */
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
         // write your code here
+
+		Arrays.sort(nums);
 
 		List<List<Integer>> ans = new ArrayList<>();
 
-		if(nums==null){
-			return ans;
-		}
-
-		backtracking(0, nums, new ArrayList<>(), ans);
+		backtrack(0, nums, new ArrayList<Integer>(), ans);
 
 		return ans;
-
     }
 
-	private void backtracking(int offset, int[] nums, List<Integer> subset, List<ArraryList<Integer>> ans){
+	private void backtrack(int offset, int[] nums, List<Integer> subset, List<List<Integer>> ans){
 
-		ans.add(new ArraryList<Integer>(subset));
+		ans.add(new ArrayList<Integer>(subset));
 
 		for(int i=offset; i<nums.length; i++){
 
+			if(i!=offset && nums[i]==nums[i-1]){
+				continue;
+			}
+
 			subset.add(nums[i]);
-			backtracking(i+1, nums, subset, ans);
-			subset.remove(subset.size()-1);
+
+				backtrack(i+1, nums, subset, ans);
+
+				subset.remove(subset.size()-1);
+
+			
 		}
 	}
 }
